0d60376aa18fa1f87d753a3f9ddd29133d338c65
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-05-05 14:51:41 -0300

    feat: :sparkles: create a auth routes and integrate with kinde

--- server/app.ts
+++ server/app.ts
@@ -3,12 +3,16 @@ import { logger } from "hono/logger";
 import { serveStatic } from "hono/bun";
 
 import { expensesRoute } from "./routes/expenses";
+import { authRoute } from "./routes/auth";
 
 const app = new Hono();
 
 app.use("*", logger());
 
-const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
+const apiRoutes = app
+  .basePath("/api")
+  .route("/expenses", expensesRoute)
+  .route("/", authRoute);
 
 app.get("*", serveStatic({ root: "./client/dist" }));
 app.get("*", serveStatic({ path: "./client/dist/index.html" }));
